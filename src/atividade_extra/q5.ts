3405210e2cbae5768c72cc3c3036557af3b05475
luiz-david05 <decd393@gmail.com>
2023-11-17 10:09:50 -0300

    fix: q5 att extra

--- src/atividade_extra/q5.ts
+++ src/atividade_extra/q5.ts
@@ -8,10 +8,7 @@ class FolhaDePagamento {
     calcularPagamentos(){
         let salarioTotal = 0
         for (let pessoa of this._pessoas) {
-            if (pessoa instanceof Professor) {
-                salarioTotal += pessoa.salario
-            }
-            else if (pessoa instanceof Funcionario) {
+            if (pessoa instanceof Funcionario) {
                 salarioTotal += pessoa.salario
             }
         }
@@ -23,8 +20,3 @@ class FolhaDePagamento {
         this._pessoas.push(pessoa)
     }
 }
-
-// let folhaDePagamento: FolhaDePagamento =  new FolhaDePagamento()
-// folhaDePagamento.inserir(new Funcionario('Luiz', 'David', '0001', 1320))
-// folhaDePagamento.inserir(new Professor('Camila', 'Torres', '0001', 4000, 'efetivo'))
-// console.log(`\nTotal de salários dos funcionários e professores: ${folhaDePagamento.calcularPagamentos()}`)
